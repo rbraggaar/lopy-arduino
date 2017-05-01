3b6ecc3233bddea441fdd7e19c88ad1488d84ceb
Rob Braggaar <r.c.braggaar@outlook.com>
2017-05-01 16:08:30 +0200

    small cleanup

--- lopy/flash/boot.py
+++ lopy/flash/boot.py
@@ -1,22 +1,19 @@
-"""Enable serial communication with the Lopy.
+"""Setup serial communication between Lopy, Arduino and PC.
 Disable LED.
 """
 import os
-
-import pycom
 import machine
-from network import WLAN
+import pycom
+
 
 # Configure first UART bus to see the communication on the pc
 uart = machine.UART(0, 115200)
 os.dupterm(uart)
-# Configure second UART bus on pins P3(TX1) and P4(RX1)
-uart1 = UART(1, baudrate=9600)
 
-wl = WLAN()
+# Configure second UART bus on pins P3(TX1) and P4(RX1)
+uart1 = machine.UART(1, baudrate=9600)
 
 pycom.heartbeat(False)
 
 machine.main('main.py')
 print('==========Starting main.py==========\n')
-
--- lopy/flash/main.py
+++ lopy/flash/main.py
@@ -1,5 +1,5 @@
-"""Sends a string to the Arduino. Waits a bit and then reads the answer from 
-the Arduino.
+"""Sends a string to the Arduino. Waits one second and then reads the answer
+from the Arduino.
 """
 import time
 
@@ -7,7 +7,7 @@ import time
 print('Main start')
 
 while True:
-	uart1.write('Hello Arduino')
-	time.sleep(1)
-	print(uart1.readline()) # read the response from the Arduino
-	time.sleep(1)
+    uart1.write('Hello Arduino')
+    time.sleep(1)
+    print(uart1.readline())  # read the response from the Arduino
+    time.sleep(1)
